2bf68ac80851d8340258ab1eba56d02279ce4671
gabryel7539 <pgabryel144@gmail.com>
2025-06-03 20:28:30 -0300

    Add files via upload

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Admin
+    public static void main(String[] args) {
+        try{
+            
+            int x = 1/0;
+        }
+        catch(ArithmeticException ae){
+            System.out.println(ae.getMessage());
+        }
+        System.out.println("Continuou a execucao...");
+        }
+    }
+
+
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        try{
+            
+            int x = 1/0;
+        }
+        catch(ArithmeticException ae){
+            System.out.println(ae.getMessage());
+        }
+        System.out.println("Continuou a execucao...");
+        }
+    }
+
